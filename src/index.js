b390e413f638dc2798b1275a4e8c6535df955cec
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-03-31 21:42:55 -0400

    home page

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import App from './Components/App';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
